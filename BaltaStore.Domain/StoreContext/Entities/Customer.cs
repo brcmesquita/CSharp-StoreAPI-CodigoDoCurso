30410084402d00aacdbc90e724844652c50880c7
Bruno Raphael Cabral de Mesquita <brcmesquita@gmail.com>
2021-01-25 21:49:59 -0300

    Fim aula 13

---
+++ BaltaStore.Domain/StoreContext/Entities/Customer.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace BaltaStore.Domain.StoreContext.Entities
+{
+    public class Customer
+    {
+        // método construtor, que nem no Python
+        public Customer(
+            string firstName, string lastName, string document,
+            string email, string phone, string address)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Document = document;
+            Email = email;
+            Phone = phone;
+            Address = address;
+        }
+
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string Document { get; private set; }
+        public string Email { get; private set; }
+        public string Phone { get; private set; }
+        public string Address { get; private set; }
+    }
+}
--- BaltaStore.Domain/StoreContext/Entities/OrderItem.cs
+++ BaltaStore.Domain/StoreContext/Entities/OrderItem.cs
@@ -1,10 +1,10 @@
 using System;
 
-namespace BaltaStore.Domain.StoreContext
+namespace BaltaStore.Domain.StoreContext.Entities
 {
     public class OrderItem
     {
-        public Product Product { get; set; }
+        public Products Product { get; set; }
         public string Quantity { get; set; }
         public string Price { get; set; }
     }
